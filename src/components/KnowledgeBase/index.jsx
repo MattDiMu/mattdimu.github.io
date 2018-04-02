9b55cf5594c65d36ca4eeb564f68e0f0e1731960
Matthias Müller <mattdimu@MattMac.local>
2018-04-02 20:14:53 +0200

    add filter, still wip

--- src/components/KnowledgeBase/index.jsx
+++ src/components/KnowledgeBase/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Radio, RadioGroup } from 'react-radio-group';
+import moment from 'moment';
 import './style.scss';
 import BaseEntry from '../BaseEntry';
 import data from '../../data/knowledge-base';
+
 
 class KnowledgeBase extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
+      selectedDate: 'all',
+      availableTags: {},
+      selectedTag: null,
       entries: []
     };
   }
@@ -16,41 +22,100 @@ class KnowledgeBase extends React.Component {
     setTimeout(() => {
       this.setState({
         isLoading: false,
-        entries: data.entries
+        entries: data.entries,
+        availableTags: data.availableTags
       });
     }, 100);
   }
 
+  handleDateChange(val) {
+    this.setState({
+      selectedDate: val
+    });
+  }
+
+  handleTagChange(val) {
+    this.setState({
+      selectedTag: val
+    });
+  }
+
   render() {
+    const availableTags = [
+      <label key="all">
+        <Radio value="" />
+        <span className="knowledge-base__circle" />
+        <span className="knowledge-base__label">Show all</span>
+      </label>
+    ];
+    Object.keys(this.state.availableTags).forEach((key) => {
+      availableTags.push(<label key={key}>
+        <Radio value={key} />
+        <span className="knowledge-base__circle" />
+        <span className="knowledge-base__label">{this.state.availableTags[key]}</span>
+      </label>); //eslint-disable-line
+    });
+
     const items = [];
     this.state.entries.forEach((item, index) => {
-      items.push(<BaseEntry key={`${index}_item.title`} title={item.title} />); // eslint-disable-line
+      // filter by date
+      if (this.state.selectedDate !== 'all') {
+        if (this.state.selectedDate === 'deprecated') {
+          if (moment().subtract(3, 'years').isBefore(moment(item.date))) {
+            return;
+          }
+        } else if (moment().subtract(this.state.selectedDate, 'years').isAfter(moment(item.date))) {
+          return;
+        }
+      }
+      // filter by tag
+      if (this.state.selectedTag) {
+        if (!item.tags || !item.tags.includes(this.state.selectedTag)) {
+          return;
+        }
+      }
+      items.push(<BaseEntry key={`${index}_item.title`} item={item} />); // eslint-disable-line
     });
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleTagChange = this.handleTagChange.bind(this);
 
     return (
       <div className="knowledge-base" aria-busy={this.state.isLoading}>
         <form className="knowledge-base__filter">
           <fieldset>
             <legend>Last Update</legend>
-            <label htmlFor="last_update_99">
-              <input type="radio" name="last_update" id="last_update_99" value="99" />
-              <span>all</span>
-            </label>
-            <label htmlFor="last_update_1">
-              <input type="radio" name="last_update" id="last_update_1" value="1" />
-              <span>within last year</span>
-            </label>
-            <label htmlFor="last_update_3">
-              <input type="radio" name="last_update" id="last_update_3" value="3" />
-              <span>within last 3 years</span>
-            </label>
-            <label htmlFor="last_update_deprecated">
-              <input type="radio" name="last_update" id="last_update_deprecated" value="deprecated" />
-              <span>Older than 3 years</span>
-            </label>
+            <RadioGroup className="knowledge-base__date" name="selectedDate" selectedValue={this.state.selectedDate} onChange={this.handleDateChange}>
+              <label>
+                <Radio value="all" />
+                <span className="knowledge-base__circle" />
+                <span className="knowledge-base__label">Show All</span>
+              </label>
+              <label>
+                <Radio value="1" />
+                <span className="knowledge-base__circle" />
+                <span className="knowledge-base__label">last year</span>
+              </label>
+              <label>
+                <Radio value="3" />
+                <span className="knowledge-base__circle" />
+                <span className="knowledge-base__label">last 3 Years</span>
+              </label>
+              <label>
+                <Radio value="deprecated" />
+                <span className="knowledge-base__circle" />
+                <span className="knowledge-base__label">older than 3 years</span>
+              </label>
+            </RadioGroup>
+          </fieldset>
+          <fieldset>
+            <legend>Topic</legend>
+            <RadioGroup className="knowledge-base__tags" name="selectedTag" selectedValue={this.state.selectedTag} onChange={this.handleTagChange}>
+              {availableTags}
+            </RadioGroup>
           </fieldset>
         </form>
-        { !this.state.isLoading && items }
+
+        { !this.state.isLoading && items.length > 0 && <ul className="knowledge-base__result">{items}</ul> }
       </div>
     );
   }
--- src/data/knowledge-base.js
+++ src/data/knowledge-base.js
@@ -1,10 +1,114 @@
 export default {
+  availableTags: {
+    javascript: 'JavaScript',
+    browser: 'Browser',
+    java: 'Java',
+    magnolia: 'Magnolia',
+    network: 'Network'
+  },
   entries: [
+    {
+      date: '2018-02-02',
+      tags: [
+        'javascript',
+        'browser'
+      ],
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      tags: [
+        'java'
+      ],
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
+    {
+      date: '2018-02-02',
+      title: 'Titel',
+      description: 'some desc'
+    },
     {
       date: '2018-02-02',
       title: 'Titel',
       description: 'some desc'
     }
-  ],
-  metadata: {}
+
+  ]
 };
