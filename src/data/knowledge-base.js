51cd1e90f34471b6ba11c3381546c6dae3905768
Matthias Müller <MattDiMu@users.noreply.github.com>
2018-04-03 18:38:25 +0200

    Update knowledge-base.js

--- src/data/knowledge-base.js
+++ src/data/knowledge-base.js
@@ -4,111 +4,50 @@ export default {
     browser: 'Browser',
     java: 'Java',
     magnolia: 'Magnolia',
-    network: 'Network'
+    network: 'Network',
+    osx: 'OSX',
+    visualization: 'Visualization',
+    ux: 'User Experience',
+    'package-manager': 'Package Manager'
   },
   entries: [
     {
-      date: '2018-02-02',
+      date: '2018-04-01',
       tags: [
-        'javascript',
-        'browser'
+        'osx',
+        'package-manager'
       ],
-      title: 'Titel',
-      description: 'some desc'
+      links: [
+        'https://brew.sh/',
+        'https://caskroom.github.io/'
+      ],
+      title: 'Homebrew & Cask',
+      description: ''
     },
     {
-      date: '2018-02-02',
+      date: '2018-04-01',
       tags: [
-        'java'
+        'package-manager'
       ],
-      title: 'Titel',
-      description: 'some desc'
+      links: [
+        'https://hackernoon.com/im-harvesting-credit-card-numbers-and-passwords-from-your-site-here-s-how-9a8cb347c5b5'
+      ],
+      title: 'Sensitive Data Harvesting',
+      description: 'About the architecture of npm & yarn and its problems'
     },
     {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
-    },
-    {
-      date: '2018-02-02',
-      title: 'Titel',
-      description: 'some desc'
+      date: '2018-04-02',
+      tags: [
+        'visualization',
+        'ux'
+      ],
+      links: [
+        'https://www.darkhorseanalytics.com/blog/data-looks-better-naked',
+        'https://www.darkhorseanalytics.com/blog/data-looks-better-naked-maps-edition',
+        'https://www.darkhorseanalytics.com/blog/salvaging-the-pie'
+      ],
+      title: 'Data looks better naked',
+      description: 'recommendations for minimalistic data representations'
     }
-
   ]
 };
