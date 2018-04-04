5f474bce031870b16d9c304d863e1b8543c89500
Matthias Müller <matthiasundmueller@gmail.com>
2018-04-04 23:14:43 +0200

    move meta descriptions

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -10,6 +10,23 @@ class Layout extends React.Component {
       <div className="layout">
         <Helmet defaultTitle="">
           <html lang="en" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Knowledge Base by Matthias Müller" />
+          <meta property="og:site_name" content="Matthias Müller" />
+          <meta property="og:title" content="Knowledge Base" />
+          <meta property="og:url" content="https://www.matthiasmueller.me" />
+          <meta property="og:locale" content="en_US" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@MattDiMu" />
+          <meta name="twitter:url" content="https://www.matthiasmueller.me" />
+          <meta name="twitter:title" content="Knowledge Base" />
+          <meta name="theme-color" content="#1976d2" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+
         </Helmet>
         {children()}
       </div>
