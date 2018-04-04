7f74220f5465950e198eb66a04d7d6ab9119c3b4
Matthias Müller <matthiasundmueller@gmail.com>
2018-04-04 22:21:30 +0200

    optimize social media integration

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -20,6 +20,18 @@ class ArticlesRoute extends React.Component {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="Knowledge Base by Matthias Müller" />  
+          <meta property="og:site_name" content="Matthias Müller" />
+          <meta property="og:title" content="Knowledge Base" />
+          <meta property="og:url" content="https://www.matthiasmueller.me" />
+          <meta property="og:locale" content="en_US" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@MattDiMu" />
+          <meta name="twitter:url" content="https://www.matthiasmueller.me" />
+          <meta name="twitter:title" content="Knowledge Base" />
+          <meta name="theme-color" content="#5d93ff" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
 
         </Helmet>
         <Sidebar {...this.props} />
