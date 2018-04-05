8d09f2df255236b116af4d7e988a36a5e568410c
Matthias Müller <MattDiMu@users.noreply.github.com>
2018-04-05 11:32:28 +0200

    Update index.jsx

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -23,6 +23,7 @@ class Layout extends React.Component {
           <meta name="twitter:title" content="Knowledge Base" />
           <meta name="theme-color" content="#1976d2" />
           <meta name="google-site-verification" content="FOZaQe5QDrL73m3dsGPGE8gybcFBPamUOa-9nIydYOY" />
+          <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
