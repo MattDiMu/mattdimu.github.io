7fcfd2bc988212999eb44700f977b27c3a60e98f
Matthias Müller <MattDiMu@users.noreply.github.com>
2018-04-10 12:37:40 +0200

    Create oss-list.js

---
+++ src/data/oss-list.js
@@ -0,0 +1,18 @@
+export default {
+  availableTags: {
+    mail: 'E-Mail',
+    framework: 'Framework',
+    javascript: 'JavaScript'
+  },
+  entries: [
+    {
+      date: '2018-04-10',
+      tags: [
+        'mail',
+        'framework'
+      ,
+      title: 'mjml',
+      desc: 'responsive email framework'
+    }
+  ]
+}
