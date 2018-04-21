c7aa2b5aacfa9a2ec402b22161b7b19a09589f2a
Matthias Müller <matthiasundmueller@gmail.com>
2018-04-21 12:02:30 +0200

    add lesser usefull courses

---
+++ src/data/knowledge-base-less-usefull.js
@@ -0,0 +1,16 @@
+export default {
+  entries: [
+    {
+      date: '2018-03-10',
+      tags: [
+        'network',
+        'browser'
+      ],
+      links: [
+        'https://eu.udacity.com/course/client-server-communication--ud897'
+      ],
+      title: 'Udacity Client Server Communication',
+      description: ''
+    }
+  ]
+};
