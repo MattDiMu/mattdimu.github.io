8a8eb1b8440ec4a95bc965e3cafb6308c60388cd
Matthias Müller <dev@matthiasmueller.me>
2019-02-05 09:03:10 +0100

    Update index.jsx

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -15,17 +15,17 @@ class Links extends React.Component {
             </a>
           </li>
           <li className="links__list-item">
-            <a href={`https://www.github.com/${author.github}`} target="_blank" rel="noopener" ><span className="visuallyhidden">Github</span>
+            <a href={`https://www.github.com/${author.github}`} target="_blank" rel="noopener noreferrer" ><span className="visuallyhidden">Github</span>
               <i className="icon-github" />
             </a>
           </li>
           <li className="links__list-item">
-            <a href={`https://www.twitter.com/${author.twitter}`} target="_blank" rel="noopener" ><span className="visuallyhidden">Twitter</span>
+            <a href={`https://www.twitter.com/${author.twitter}`} target="_blank" rel="noopener noreferrer" ><span className="visuallyhidden">Twitter</span>
               <i className="icon-twitter" />
             </a>
           </li>
           <li className="links__list-item">
-            <a href="https://stackoverflow.com/users/3815374/mattdimu" target="_blank" rel="noopener" ><span className="visuallyhidden">Stackoverflow</span> {/* eslint-disable-line */}
+            <a href="https://stackoverflow.com/users/3815374/mattdimu" target="_blank" rel="noopener noreferrer" ><span className="visuallyhidden">Stackoverflow</span> {/* eslint-disable-line */}
               <i className="icon-stackoverflow" />
             </a>
           </li>
